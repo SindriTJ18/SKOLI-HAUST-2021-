c781853933ee21853ed330ffd8bcea085d3ef5f2
SindriTJ18 <89155672+SindriTJ18@users.noreply.github.com>
2021-08-27 23:06:14 +0000

    Add files via upload

---
+++ Skóli/GAGNogVIT/T809DATA_2021/01_decision_trees/template.py
@@ -0,0 +1,141 @@
+#from _typeshed import Self
+from typing import Union, ValuesView
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.lib.shape_base import split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
+from tools import load_iris, split_train_test
+
+def prior(targets: np.ndarray, classes: list) -> np.ndarray:
+    bin = np.zeros(len(classes))
+    for i in classes:
+        for j in targets:
+            if i == j:
+                bin[i] += 1/len(targets)
+    return bin
+
+def split_data(
+    features: np.ndarray,
+    targets: np.ndarray,
+    split_feature_index: int,
+    theta: float
+) -> Union[tuple, tuple]:
+
+    features_1 = []
+    targets_1 = []
+
+    features_2 = []
+    targets_2 = []
+
+    for i in range(len(features)):
+        if features[i][split_feature_index] < theta:
+            features_1.append(features[i])
+            targets_1.append(targets[i])
+        else:
+            features_2.append(features[i])
+            targets_2.append(targets[i])
+
+    return (np.array(features_1), np.array(targets_1)), (np.array(features_2), np.array(targets_2))
+
+def gini_impurity(targets: np.ndarray, classes: list) -> float:
+    dogs = prior(targets,classes)
+    return 0.5*(1-np.sum(np.power(dogs,2)))
+
+
+def weighted_impurity(
+    t1: np.ndarray,
+    t2: np.ndarray,
+    classes: list
+) -> float:
+    g1 = gini_impurity(t1,classes)
+    g2 = gini_impurity(t2,classes)
+    n = t1.shape[0] + t2.shape[0]
+    N1 = len(t1)
+    N2 = len(t2)
+
+    return ((N1*g1)+(N2*g2))/n
+
+
+def total_gini_impurity(
+    features: np.ndarray,
+    targets: np.ndarray,
+    classes: list,
+    split_feature_index: int,
+    theta: float
+) -> float:
+    (f_1, t_1), (f_2, t_2) = split_data(features,targets,split_feature_index,theta)
+    return weighted_impurity(t_1,t_2,classes)
+
+def brute_best_split(
+    features: np.ndarray,
+    targets: np.ndarray,
+    classes: list,
+    num_tries: int
+) -> Union[float, int, float]:
+    best_gini, best_dim, best_theta = float("inf"), None, None
+    # iterate feature dimensions
+    for i in range(features.shape[1]):
+        # create the thresholds
+        thetas = np.linspace(np.min(features[:,i]), np.max(features[:,i]), num_tries+2)[1:-1]
+
+        # iterate thresholds
+        for theta in thetas:
+            total_gini = total_gini_impurity(features, targets, classes, i, theta)
+            if total_gini < best_gini:
+                best_gini = total_gini
+                best_dim = i
+                best_theta = theta
+    
+    return best_gini, best_dim, best_theta
+
+
+class IrisTreeTrainer:
+    def __init__(
+        self,
+        features: np.ndarray,
+        targets: np.ndarray,
+        classes: list = [0, 1, 2],
+        train_ratio: float = 0.8
+    ):
+        (self.train_features, self.train_targets),\
+            (self.test_features, self.test_targets) =\
+            split_train_test(features, targets, train_ratio)
+
+        self.classes = classes
+        self.tree = DecisionTreeClassifier()
+
+    def train(self):
+        return self.tree.fit(self.train_features,self.train_targets)
+
+    def accuracy(self):
+        predict = self.tree.predict(self.test_features)
+        return accuracy_score(self.test_targets, predict)
+
+    def plot(self):
+        plot_tree(self.tree)
+        fig1 = plt.gcf()
+        plt.show()
+        fig1.savefig("2_3_2.png")
+
+    def plot_progress(self):
+        # Independent section
+        # Remove this method if you don't go for independent section.
+        ...
+
+    def guess(self):
+        pred = self.tree.predict(self.test_features)
+        return pred
+
+    def confusion_matrix(self):
+        n = len(self.classes)
+        M = np.zeros((n,n))
+
+        guess = self.guess()
+        for i in range(len(self.test_targets)):
+            j = self.test_targets[i]
+            k = guess[i]
+            M[j][k] += 1
+        return M
+
---
+++ Skóli/GAGNogVIT/T809DATA_2021/02_nearest_neighbours/template.py
@@ -0,0 +1,174 @@
+import numpy as np
+import collections
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from numpy.lib.function_base import append
+from tools import load_iris, split_train_test, plot_points
+
+def remove_one(points: np.ndarray, i: int):
+    return np.concatenate((points[0:i], points[i+1:]))
+
+def euclidian_distance(x: np.ndarray, y: np.ndarray) -> float:
+    dist = np.linalg.norm(x - y)
+    return dist
+
+def euclidian_distances(x: np.ndarray, points: np.ndarray) -> np.ndarray:
+    dists = []
+    for i in range(len(points)):
+        dists.append(euclidian_distance(x,points[i]))
+    return dists
+
+def k_nearest(x: np.ndarray, points: np.ndarray, k: int):
+    distances = euclidian_distances(x, points)
+    order = np.argsort(distances)
+    return order[:k]
+
+def vote(targets, classes):
+    TelVect = []
+    for i in range(len(classes)):
+        counter=0
+        for j in range(len(targets)):
+            if targets[j] == classes[i]:
+                counter += 1     
+        TelVect.append(counter)
+    MaxVal = TelVect.index(max(TelVect))
+    Common = classes[MaxVal]
+    return Common
+
+def knn(
+    x: np.ndarray,
+    points: np.ndarray,
+    point_targets: np.ndarray,
+    classes: list,
+    k: int
+) -> np.ndarray:
+    nearest = k_nearest(x, points, k)
+    # SKILAR INDEX Á "K" CLOSEST POINTS
+    voting = vote(point_targets[nearest], classes)
+    return voting
+
+def knn_predict(
+    points: np.ndarray,
+    point_targets: np.ndarray,
+    classes: list,
+    k: int
+) -> np.ndarray:
+    arr = []
+    for i in range(len(point_targets)):
+        temp_pts = remove_one(points, i)
+        temp_trg = remove_one(point_targets, i)
+        arr.append(knn(points[i],temp_pts,temp_trg,classes,k))
+    return np.array(arr)
+
+
+def knn_accuracy(
+    points: np.ndarray,
+    point_targets: np.ndarray,
+    classes: list,
+    k: int
+) -> float:
+    pred = knn_predict(points,point_targets,classes,k)
+    return accuracy_score(point_targets,pred)
+    
+
+
+def knn_confusion_matrix(
+    points: np.ndarray,
+    point_targets: np.ndarray,
+    classes: list,
+    k: int
+) -> np.ndarray:
+    n = len(classes)
+    M = np.zeros((n,n))
+
+    guess = knn_predict(points,point_targets,classes,k)
+    for i in range(len(point_targets)):
+        j = point_targets[i]
+        k = guess[i]
+        M[k][j] += 1
+    return M
+
+
+def best_k(
+    points: np.ndarray,
+    point_targets: np.ndarray,
+    classes: list,
+) -> int:
+    accuracy = 0
+    best_accuracy = 0
+    for i in range(len(points)-1):
+        accuracy = knn_accuracy(points, point_targets, classes, i)
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_k = i
+    return best_k
+
+
+def knn_plot_points(
+    points: np.ndarray,
+    point_targets: np.ndarray,
+    classes: list,
+    k: int
+):
+    colors = ['yellow', 'purple', 'blue']
+    prediction = knn_predict(points, point_targets, classes, k)
+    for i in range(points.shape[0]):
+        [x, y] = points[i,:2]
+        if point_targets[i] == prediction[i]:
+            edgestring = 'green'
+        else:
+            edgestring = 'red' 
+        plt.scatter(x, y, c=colors[point_targets[i]], edgecolors=edgestring,
+            linewidths=2)
+    plt.title('Yellow=0, Purple=1, Blue=2')
+    fig1 = plt.gcf()
+    plt.show()
+    fig1.savefig("2_5_1.png")
+
+
+def weighted_vote(
+    targets: np.ndarray,
+    distances: np.ndarray,
+    classes: list
+) -> int:
+    '''
+    Given a list of nearest targets, vote for the most
+    popular
+    '''
+    # Remove if you don't go for independent section
+    ...
+
+
+def wknn(
+    x: np.ndarray,
+    points: np.ndarray,
+    point_targets: np.ndarray,
+    classes: list,
+    k: int
+) -> np.ndarray:
+    '''
+    Combine k_nearest and vote
+    '''
+    # Remove if you don't go for independent section
+    ...
+
+
+def wknn_predict(
+    points: np.ndarray,
+    point_targets: np.ndarray,
+    classes: list,
+    k: int
+) -> np.ndarray:
+    # Remove if you don't go for independent section
+    ...
+
+
+def compare_knns(
+    points: np.ndarray,
+    targets: np.ndarray,
+    classes: list
+):
+    # Remove if you don't go for independent section
+    ...
+d, t, classes = load_iris()
+knn_plot_points(d, t, classes, 3)
